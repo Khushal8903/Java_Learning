e6dce3aa1f1724933264f05d277bc28e1fc3d88f
Khushal8903 <147701048+Khushal8903@users.noreply.github.com>
2026-04-03 22:35:33 +0530

    update multithread

--- 9_Multithreading/Q1.java
+++ 9_Multithreading/Q1.java
@@ -1,46 +1,19 @@
-class BankAccount
-{   private int balance=100;
-   public synchronized void updateBalance(int amount)
-   {
-       int temp=balance;
-	   System.out.println(Thread.currentThread().getName()+"  reads balance "+balance);
-	   temp=temp+amount;
-	   try{
-	      Thread.sleep(200);
-	   }
-	   catch(Exception ex)
-	   {  System.out.println(ex);
-	   }
-	   balance=temp;
-System.out.println(Thread.currentThread().getName()+"  Writes new  balance "+balance);
-   }
-   public int getBalance()
-   { return balance;
-   } 
+class Test extends Thread{
+	
+		public void run(){
+			try{
+			for(int i=1;i<=5 ;i++){
+				System.out.println(i);
+				Thread.sleep(1000);
+			}			
+		}catch(Exception e){
+		System.out.println(e);
+	}
+		}
 }
-class Trans extends Thread 
-{  BankAccount bankAccount;
-   int value;
-   Trans(BankAccount bankAccount,int value)
-   { this.bankAccount=bankAccount;
-     this.value=value;
-   }
-   public void run()
-   {  bankAccount.updateBalance(value);
-   }
-}
-public class Q1
-{ public static void main(String x[])throws Exception
-  {  BankAccount account=new BankAccount();
-      Trans deposit=new Trans(account,50);
-	  deposit.setName("Deposit");
-	  
-	  Trans wid=new Trans(account,-50);
-	  wid.setName("Widraw");
-	  deposit.start();
-	  wid.start();
-	  deposit.join();
-	  wid.join();
-	  System.out.println("final balance  "+account.getBalance());
-  }  
+public class Q1 {
+	public static void main(String x[]){
+	Test t = new Test();
+	t.start();
+	}
 }
